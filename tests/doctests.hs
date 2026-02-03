ef7e3f507908a7ae582c257c22ba4168dc68be2b
Viktor Sonesten <v@tmplt.dev>
2026-02-03 12:37:05 +0100

    doctest: try explicitly calling the mwe
    Result: the explicit call works, but not mainFromCabal.

--- tests/doctests.hs
+++ tests/doctests.hs
@@ -2,6 +2,9 @@ module Main where
 
 import System.Environment (getArgs)
 import Test.DocTest (mainFromCabal)
+import MWE (mwe)
 
 main :: IO ()
-main = mainFromCabal "mwe" =<< getArgs
+main = do
+     putStrLn . show $ mwe
+     mainFromCabal "mwe" =<< getArgs
